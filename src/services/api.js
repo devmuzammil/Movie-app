c0f5cc9f6d99b59b7d224e4dba609b933e258426
Muzammil Hussain <muzammilhussainabro@gmail.com>
2025-05-02 19:19:13 +0500

    Finish remaining part of project

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "7a9522a4f04f45c3b608a81dc12cbca5";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
